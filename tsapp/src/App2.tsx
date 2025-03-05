f7567700ccd38e8dde549bd78f4f95ec6ad9d1bd
jaemin <sjmjm4@naver.com>
2025-03-05 21:42:21 +0900

    feat: 20250305 fetch / axios api started

---
+++ tsapp/src/App2.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert(`안녕, ${name}!`);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' onChange={handleChange} value={name} />
+      <input type='submit' value='제출' />
+    </form>
+  )
+}
+
+export default App
